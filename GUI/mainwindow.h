3d8e6d2812ef4e06096c5a067be467bdf771f7d8
Sami Ljimari <48930092+samiljimari@users.noreply.github.com>
2020-02-08 10:41:18 +0000

    GUI files upload

---
+++ GUI/mainwindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFile>
+#include <QFileDialog>
+
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_treeView_activated(const QModelIndex &index);
+
+    void on_pushButton_clicked();
+
+    void on_listView_activated(const QModelIndex &index);
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
